28e29c1022732f66dc6b48d7fbb788d6b16a266f
param-ninthu <ninthu1999@gmail.com>
2023-03-04 23:42:05 +0530

    header

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -11,13 +11,16 @@ const Hero = ({ movies }) => {
           return (
             <Paper>
               <div className="movie-card-container">
-                <div className="movie-card">
+                <div
+                  className="movie-card"
+                  style={{ "--img": `url(${movie.backdrops[0]})` }}
+                >
                   <div className="movie-detail">
                     <div className="movie-poster">
                       <img src={movie.poster} />
                     </div>
                     <div>
-                      <h4>{movie.title}</h4>
+                      <h4 className="movie-title">{movie.title}</h4>
                     </div>
                   </div>
                 </div>
