5074112aee5e3c698ffcd0779fa5eb0d2669dcc9
param-ninthu <ninthu1999@gmail.com>
2023-03-04 15:08:34 +0530

    axios change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <> Hello </>;
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <>{movies}</>;
 }
 
 export default App;
